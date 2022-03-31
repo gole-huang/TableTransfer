bc8740db69ef1f78a547896ff5af0918e3701b01
gole.huang <gole.huang@gmail.com>
2022-03-31 16:04:14 +0800

    MySQL Finished

--- importDB.cs
+++ importDB.cs
@@ -1,6 +1,4 @@
 using System.Data;
-using System.Linq;
-using System.IO;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using MySqlConnector;
@@ -53,26 +51,50 @@ namespace importExceltoDB
             xlsxName = XLSXFile;
             dt = new DataTable();
         }
-        /*
-        private DataTable readFromExcel()
+        private static object GetValueType(ICell iCell)
+        {   //获取
+            if (iCell == null) return null;
+            switch (iCell.CellType)
+            {
+                case CellType.Blank:
+                    return null;
+                case CellType.Boolean:
+                    return iCell.BooleanCellValue;
+                case CellType.Numeric:
+                    return iCell.NumericCellValue;
+                case CellType.String:
+                    return iCell.StringCellValue;
+                case CellType.Formula:
+                    return iCell.NumericCellValue;
+                case CellType.Error:
+                    return iCell.ErrorCellValue;
+                default:
+                    return iCell.CellFormula;
+            }
+        }
+        /**/
+        private async DataTable readFromExcel()
         {
+            DataTable dt = new DataTable();
             using (FileStream fs = new FileStream(xlsxName, FileMode.Open, FileAccess.Read))
             {
                 IWorkbook wb = new XSSFWorkbook(fs);
                 ISheet iSheet = wb.GetSheetAt(0);
-                IRow IRow = iSheet.GetRow(iSheet.FirstRowNum);
-                for (int i = 0; i < IRow.LastCellNum; i++)
-                    ds.Tables[0]
+                IRow iRow = iSheet.GetRow(iSheet.FirstRowNum);
+                for (int i = 0; i < iRow.LastCellNum; i++)
+                    dt.Columns.Add(new DataColumn(GetValueType(iRow.GetCell(i)).ToString()));
             }
         }
-        */
         private DataTable readFromMySQL()
         {
             DataTable dt = new DataTable();
             string MySqlCmd = "Select OLD_IP , NEW_IP , NEW_MASK , NEW_GW , NEW_DNS from IP_RELATIONSHIP";
             using (MySqlDataAdapter dbAdapter = new MySqlDataAdapter(MySqlCmd, String.Join(';', connString)))
             {
-                dbAdapter.Fill(dt);
+                using (StreamWriter sw = new StreamWriter(logName))
+                {
+                    sw.WriteLine("From MySQL read " + dbAdapter.Fill(dt) + " lines.");
+                }
             }
             return dt;
         }
@@ -90,11 +112,14 @@ namespace importExceltoDB
                 MySqlBulkCopy MySqlBC = new MySqlBulkCopy(MySqlConn);
                 MySqlBC.DestinationTableName = "IP_RELATIONSHIP";
                 MySqlBulkCopyResult result = await MySqlBC.WriteToServerAsync(dt);
-                if (result.Warnings.Count != 0)
+                using (StreamWriter sw = new StreamWriter(logName))
                 {
-                    using (StreamWriter sw = new StreamWriter(logName))
-                    foreach ( var w in result.Warnings )
-                        sw.WriteLine(w.ToString());
+                    if (result.Warnings.Count != 0)
+                    {
+                        foreach (var w in result.Warnings)
+                            sw.WriteLine(w.ToString());
+                    }
+                    sw.WriteLine("To MySQL write " + result.RowsInserted + " lines.");
                 }
             }
         }
